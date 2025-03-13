2adc14e565a1bdc1ee3ffc76eb83ae1116f44e31
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-13 13:28:29 -0600

    Issue #3
    A slightly more complicated example.

--- scrape_training_data.py
+++ scrape_training_data.py
@@ -1,4 +1,5 @@
 import os
+import json
 import requests
 from dotenv import load_dotenv
 
@@ -7,13 +8,22 @@ def main():
     marketstack_api_key = os.getenv("MARKETSTACK_API_KEY")
 
     # With query parameters
-    params = {"access_key": marketstack_api_key, "symbols": "AAPL"}
-    response = requests.get("http://api.marketstack.com/v2/eod/latest", params=params)
+    params = {
+        "access_key": marketstack_api_key,
+        "symbols": "AAPL",
+        "date_from": "2025-01-02",
+        "date_to": "2025-03-12",
+        "limit": 100,
+    }
+    response = requests.get("http://api.marketstack.com/v2/eod", params=params)
 
     # Check if the request was successful
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
+        output_filename = os.path.join("output", "latest_request.json")
+        with open(output_filename, "w") as file:
+            json.dump(data, file, indent=4, sort_keys=True)
         print(data)
     else:
         print(f"Request failed with status code: {response.status_code}")
