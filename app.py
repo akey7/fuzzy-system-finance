d6a760c5a18df9e5a758d7440bf26537f1ce457a
Alicia Key <2609896+akey7@users.noreply.github.com>
2025-03-19 15:54:12 -0600

    Issue #13
    A boiler plate app to get started.

---
+++ app.py
@@ -0,0 +1,12 @@
+import gradio as gr
+
+
+def main():
+    with gr.Blocks() as app:
+        gr.Markdown("fuzzy-system-finance coming soon!")
+
+    app.launch(share=False)
+
+
+if __name__ == "__main__":
+    main()
